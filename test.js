8ab00372e7f595be7cea9c1ed0c2ce5d725bf5f4
YS2226 <ysatake@umass.edu>
2023-09-23 21:42:29 -0400

    centeralize

---
+++ test.js
@@ -0,0 +1,10 @@
+// Generate a random integer between min and max (inclusive)
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Example usage: generate a random number between 1 and 100
+const randomNum = getRandomInt(1, 100);
+console.log(randomNum);
